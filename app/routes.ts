d8623c892c4f66a87450b028cff7042898bd0cf7
kartikeysrivastava-sde <kartikey.srivastava@fynocrat.com>
2025-12-04 11:49:24 +0530

    root.ts , talwind output

--- app/routes.ts
+++ app/routes.ts
@@ -1,12 +1,5 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  // Catch-all for Chrome DevTools and other well-known paths
-  {
-    path: "/.well-known/*",
-    loader: () => {
-      return new Response(null, { status: 404 });
-    },
-  },
+  index("./routes/home.tsx"),
 ] satisfies RouteConfig;
